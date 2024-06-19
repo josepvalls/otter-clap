aea79277d85f37522963f4a7858dd9e97254a6a5
Josep Valls <josep@valls.name>
2024-06-18 23:07:36 -0700

    some more progress

--- scenes/protagonist.gd
+++ scenes/protagonist.gd
@@ -18,3 +18,24 @@ func _physics_process(delta):
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
+
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		var tilemap: TileMap = $"../map/NavigationRegion2D/TileMap"
+		var coords = tilemap.local_to_map(tilemap.to_local(global_position))
+		var td: TileData = tilemap.get_cell_tile_data(0, coords + Vector2i(0,-1))
+		print("accept ", coords, td)
+		tilemap.set_cell(0, coords, 2, Vector2i(5, 4))
+		var next = Vector2i.ZERO
+		if randf()<0.5:
+			next = Vector2i(-1, 0)
+		else:
+			next = Vector2i(1, 0)
+		tilemap.set_cell(0, coords+next, -1, Vector2i(5, 4))
+			
+		#tilemap.set_cells_terrain_connect
+		var region: NavigationRegion2D = $"../map/NavigationRegion2D"
+		region.bake_navigation_polygon()
+		
+		
